0fd449d8b5a49e7241c34975ac9c451c0fb7f217
schmit237u <jonathan.schmitt9@etu.univ-lorraine.fr>
2018-02-16 09:15:54 +0100

    added squelette de etat

--- src/Reversi/Etat.java
+++ src/Reversi/Etat.java
@@ -2,4 +2,7 @@ package Reversi;
 
 public abstract class Etat {
 
+	public abstract void successeur();
+	public abstract void lireEtat();
+	public abstract void ecrireEtat();
 }
--- src/Reversi/EtatReversi.java
+++ src/Reversi/EtatReversi.java
@@ -1,12 +1,32 @@
 package Reversi;
 
 public class EtatReversi extends Etat {
+	
 	private Jeu game;
 	// connait les positions grace au jeu
 	// connait les joueurs grace au jeu
+	private String joueurCourant;
+	
 	
 	public EtatReversi() {
 		
+	}
+
+	@Override
+	public void successeur() {
+		
+	}
+
+
+	@Override
+	public void lireEtat() {
+		
+	}
+
+
+	@Override
+	public void ecrireEtat() {
+
 	}
 	
 }
